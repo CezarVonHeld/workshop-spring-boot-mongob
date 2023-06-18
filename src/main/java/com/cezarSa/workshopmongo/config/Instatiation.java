02374164a1ba23a2aef0ee981a2f995c8c22eaa4
Cezar Von Held <cezarsa9@gmail.com>
2023-06-18 10:58:07 -0300

    usando padrao DTO para retornar usuarios

--- src/main/java/com/cezarSa/workshopmongo/config/Instatiation.java
+++ src/main/java/com/cezarSa/workshopmongo/config/Instatiation.java
@@ -25,6 +25,5 @@ public class Instatiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-	}
-	
+	}	
 }
