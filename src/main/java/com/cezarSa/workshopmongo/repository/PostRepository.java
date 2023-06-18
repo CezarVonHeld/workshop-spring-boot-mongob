b623adcef3a6f84a9d0ab3f87473d7e2f99e514b
Cezar Von Held <cezarsa9@gmail.com>
2023-06-18 14:41:20 -0300

    Criando entityPost com user aninhado

---
+++ src/main/java/com/cezarSa/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.cezarSa.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cezarSa.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+}
