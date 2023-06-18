6b6aa9ce961980a75dc07d694b2dfd1bd787e018
Cezar Von Held <cezarsa9@gmail.com>
2023-06-18 11:52:07 -0300

    obtendo usuario por id

--- src/main/java/com/cezarSa/workshopmongo/services/UserService.java
+++ src/main/java/com/cezarSa/workshopmongo/services/UserService.java
@@ -1,13 +1,14 @@
 package com.cezarSa.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Service;
 
 import com.cezarSa.workshopmongo.domain.User;
 import com.cezarSa.workshopmongo.repository.UserRepository;
+import com.cezarSa.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
